4e51dc043e1a3445e9a48f0287926d35fad55a43
COG IT Space <105115369+cogitspace@users.noreply.github.com>
2022-10-05 07:15:42 +0200

    Updated

--- Additional uploads/Wazuh_agent_setup_windows.ps1
+++ Additional uploads/Wazuh_agent_setup_windows.ps1
@@ -1,4 +1,2 @@
-@echo off
-
-Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.3.8-1.msi -OutFile ${env:tmp}\wazuh-agent-4.3.8.msi; msiexec.exe /i ${env:tmp}\wazuh-agent-4.3.8.msi /q WAZUH_MANAGER='176.58.114.177' WAZUH_REGISTRATION_SERVER='176.58.114.177' 
+Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.3.8-1.msi -OutFile ${env:tmp}\wazuh-agent-4.3.8.msi; msiexec.exe /i ${env:tmp}\wazuh-agent-4.3.8.msi /q WAZUH_MANAGER='<ip address of server>' WAZUH_REGISTRATION_SERVER='<ip address of server>' 
 NET START WazuhSvc
