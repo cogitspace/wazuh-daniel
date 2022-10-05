325cdb2c0cae971ccbcb883e19836d94ce89b00c
COG IT Space <105115369+cogitspace@users.noreply.github.com>
2022-10-05 15:34:35 +0200

    deleted improvements

--- improvements/windows_template.sh
+++
@@ -1,3 +0,0 @@
-Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.3.8-1.msi -OutFile ${env:tmp}\wazuh-agent-4.3.8.msi; msiexec.exe /i ${env:tmp}\wazuh-agent-4.3.8.msi /q WAZUH_MANAGER='<replace>' WAZUH_REGISTRATION_SERVER='<replace>' 
-Start-Sleep -Second 20
-NET START WazuhSvc
